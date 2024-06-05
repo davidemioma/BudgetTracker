58338205849adb013b4f9dec39cac42f5bc3fc10
david emioma <ue22aac@herts.ac.uk>
2024-06-05 22:13:07 +0100

    Setup Kinde authentication

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,17 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { serveStatic } from "hono/bun";
+import { authRoute } from "./routes/auth";
 import { expensesRoute } from "./routes/expenses";
 
 const app = new Hono();
 
 app.use("*", logger());
 
-const apiRoutes = app.basePath("/api").route("/expenses", expensesRoute);
-
+const apiRoutes = app
+  .basePath("/api")
+  .route("/auth", authRoute)
+  .route("/expenses", expensesRoute);
 // Frontend. it's displays the client code if no API endpoint.
 app.get("*", serveStatic({ root: "./client/dist" }));
 
