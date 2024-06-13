f8c0fff5f840bb94e58dd9ff47e580ff88f020a8
david emioma <ue22aac@herts.ac.uk>
2024-06-13 21:41:02 +0100

    set up prod database

--- migrate.ts
+++ migrate.ts
@@ -3,7 +3,12 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 
 // for migrations
-const migrationClient = postgres(process.env.DATABASE_URL || "", { max: 1 });
+const migrationClient = postgres(process.env.DATABASE_URL || "", {
+  max: 1,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 await migrate(drizzle(migrationClient), { migrationsFolder: "./drizzle" });
 
